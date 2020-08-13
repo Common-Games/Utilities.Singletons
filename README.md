5077a3effd831a88d3c34f9158283629357150b9
Walter Haynes <walter-haynes@outlook.com>
2020-08-13 15:50:28 +0200

    Added first version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Utilities.Singletons
-Set of useful Singletons for Unity
+# CommonGames.Utilities.Singletons
+A couple handy Singletons for MonoBehavious and ScriptableObjects
+
+## Requirements
+- .NET Framework 4.5
+- JSON .NET For Unity (Free) - https://assetstore.unity.com/packages/tools/input-management/json-net-for-unity-11347
+
+## Installing
+1. Go to the Package Manager (Under **Window/Package Manager**). 
+2. Click on the little + icon.
+3. Click on button that says "Add Package from git URL".
+4. Put in `https://github.com/Common-Games/Utilities.Singletons.git`.
+5. Click on [Add] and you're done! ;-)
+
+## Explanation
+Will add later.
+
+## API
+Will add later.
---
+++ Runtime/CommonGames/Utilities/Singletons/ExecuteAlwaysSingleton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+using JetBrains.Annotations;
+
+namespace CommonGames.Utilities.Singletons
+{
+    //Made by Wybren van den Akker and Walter
+    //11/12/2019
+    [ExecuteAlways]
+    public class ExecuteAlwaysSingleton<T> : MonoBehaviour where T : ExecuteAlwaysSingleton<T>
+    {
+        /// <summary> The static reference to the Instance </summary>
+		[PublicAPI]
+        public static T Instance { get; protected set; }
+
+        /// <summary> Gets whether an Instance of this singleton exists </summary>
+        [PublicAPI]
+        public static bool InstanceExists => (Instance != null);
+
+        /// <summary> OnEnable method to associate Singleton with Instance </summary>
+        protected virtual void Awake()
+        {
+            if (InstanceExists)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = (T)this;
+            }
+        }
+
+        /// <summary> OnDestroy method to clear Singleton association </summary>
+        protected virtual void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
+    }
+}
